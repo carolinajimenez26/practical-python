73f512f2c25f707cbd3e4806c50865b093d69fac
Carolina <carolina.jimenez.g@gmail.com>
2020-08-24 16:39:45 +0200

    Adds pcost problem

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,10 @@
-# pcost.py
-#
-# Exercise 1.27
+total_cost = 0
+
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
+    for line in f:
+        name, shares, cost = line.split(",")
+        print(f"name: {name}, shares: {shares}, cost: {cost}")
+        total_cost += (float(cost) * float(shares))
+
+print(f"Total cost to purchase all of the shares: {total_cost}")
