e10184f069704271900f754bf6614450875d5e1b
Carolina <carolina.jimenez.g@gmail.com>
2020-08-27 16:27:16 +0200

    Adds error handling

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,9 +1,12 @@
 import csv
 
-def parse_csv(filename, select = None, types = None, has_headers = False, delimiter = ' '):
+def parse_csv(filename, select = None, types = None, has_headers = False, delimiter = ',', silence_errors = False):
     '''
     Parse a CSV file into a list of records
     '''
+    if not has_headers and select:
+        raise RuntimeError("select argument requires column headers")
+
     with open(filename) as f:
         rows = csv.reader(f, delimiter=delimiter)
 
@@ -15,25 +18,33 @@ def parse_csv(filename, select = None, types = None, has_headers = False, delimi
         
         records = []
 
-        for row in rows:
+        for index_row, row in enumerate(rows, 1):
             if not row:    # Skip rows with no data
                 continue
 
-            if has_headers:
-                record = {header: val for header_index, (header, val) in enumerate(zip(headers, row)) if header in select }
-                if types:
-                    record = { key: func(record[key]) for func, key in zip(types, record) }
-            else:
-                record = tuple(val for val in row)
-                if types:
-                    record = tuple(func(val) for func, val in zip(types, row))
-            
+            try:
+                if has_headers:
+                    record = {header: val for header_index, (header, val) in enumerate(zip(headers, row)) if header in select }
+                    if types:
+                        record = { key: func(record[key]) for func, key in zip(types, record) }
+                else:
+                    record = tuple(val for val in row)
+                    if types:
+                        record = tuple(func(val) for func, val in zip(types, row))
+            except ValueError as err:
+                if not silence_errors:
+                    print(f"Row {index_row}: Couldn't convert {row}")
+                    print(f"Row {index_row}: Reason {err}")
+
             records.append(record)
 
     return records
 
 
-portfolio = parse_csv('Data/portfolio.dat', types=[str, int, float], has_headers=True, delimiter=' ')
+portfolio = parse_csv('Data/missing.csv', types=[str, int, float], has_headers=True, silence_errors=True)
+# portfolio = parse_csv('Data/missing.csv', types=[str, int, float], has_headers=True)
+# parse_csv('Data/prices.csv', select=['name','price'], has_headers=False) # Raises an Exception
+# portfolio = parse_csv('Data/portfolio.dat', types=[str, int, float], has_headers=True, delimiter=' ')
 # portfolio = parse_csv('Data/prices.csv', types=[str, float], has_headers=False)
 # portfolio = parse_csv('Data/portfolio.csv', select=['name', 'price'], types=[str, float], has_headers=True)
 # portfolio = parse_csv('Data/portfolio.csv', select=['name', 'prices'], types=[str, float])
