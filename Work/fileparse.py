40245fa456208a838591bfd44a55c66c3f516a4d
Carolina <carolina.jimenez.g@gmail.com>
2020-08-27 15:46:35 +0200

    Allows passing the headers we want to select, the type for each column and if the file has headers

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,38 @@
-# fileparse.py
-#
-# Exercise 3.3
+import csv
+
+def parse_csv(filename, select = None, types = None, has_headers = False):
+    '''
+    Parse a CSV file into a list of records
+    '''
+    with open(filename) as f:
+        rows = csv.reader(f)
+
+        # Read the file headers
+        if (has_headers): 
+            headers = next(rows)
+        
+        records = []
+
+        for row in rows:
+            if not row:    # Skip rows with no data
+                continue
+
+            if (has_headers):
+                record = {header: val for header_index, (header, val) in enumerate(zip(headers, row)) if header in select }
+                if types:
+                    record = { key: func(record[key]) for func, key in zip(types, record) }
+            else:
+                record = tuple(val for val in row)
+                if types:
+                    record = tuple(func(val) for func, val in zip(types, row))
+            
+            records.append(record)
+
+    return records
+
+
+# portfolio = parse_csv('Data/prices.csv', types=[str, float], has_headers=False)
+portfolio = parse_csv('Data/portfolio.csv', select=['name', 'price'], types=[str, float], has_headers=True)
+# portfolio = parse_csv('Data/portfolio.csv', select=['name', 'prices'], types=[str, float])
+# portfolio = parse_csv('Data/portfolio.csv', select=['name', 'shares'], types=[str, int])
+print(portfolio)
