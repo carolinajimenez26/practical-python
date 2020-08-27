df52094af93e322512e9418d246eba6a450af852
Carolina <carolina.jimenez.g@gmail.com>
2020-08-27 16:55:48 +0200

    Removes functions and other things that are not being used

--- Work/report.py
+++ Work/report.py
@@ -2,32 +2,13 @@ import csv
 import sys
 import fileparse
 
-def read_prices(filename):
-    prices = {}
-    f = open(filename)
-    rows = csv.reader(f)
-    types = [str,float]
-
-    for row in rows:
-        converted = [ func(val) for func, val in zip(types, row) ]
-        # print(converted)
-        try:
-            prices[converted[0]] = converted[1]
-            # print(f"prices[{row[0]}] = {prices[row[0]]}")
-        except IndexError:
-            pass
-
-    f.close()
-    return prices
-
-
 def make_report(portfolio, prices):
     report = []
 
     for stock in portfolio:
-        current_price = float(prices[stock['name']])
-        change = current_price - float(stock['price'])
-        summary = (stock['name'], int(stock['shares']), current_price, change)
+        current_price = prices[stock['name']]
+        change = current_price - stock['price']
+        summary = (stock['name'], stock['shares'], current_price, change)
         report.append(summary)
     return report
 
@@ -47,11 +28,11 @@ def portfolio_report(portfolio_file_name, prices_file_name):
     prices    = dict(fileparse.parse_csv(prices_file_name, types=[str, float]))
 
     # Calculate the total cost of the portfolio
-    total_cost = sum([int(s['shares']) * float(s['price']) for s in portfolio])
+    total_cost = sum([s['shares'] * s['price'] for s in portfolio])
     print('Total cost', total_cost)
 
     # Compute the current value of the portfolio
-    total_value = sum([int(s['shares']) * float(prices[s['name']]) for s in portfolio])
+    total_value = sum([s['shares'] * prices[s['name']] for s in portfolio])
     print('Current value', total_value)
     print('Gain', total_value - total_cost)
 
